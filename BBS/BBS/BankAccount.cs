66a95d8f5024c450cc3a476402da6ae5edd55d49
Pronoy Debnath <121173103+Pronoy-Deb@users.noreply.github.com>
2025-03-04 18:11:09 +0600

    Uploaded the project

---
+++ BBS/BBS/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBS
+{
+    public class Account
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public double InterestRate { get; set; }
+    }
+
+    class BankAccount
+    {
+        public SortedDictionary<int, Account> UserInfo = new SortedDictionary<int, Account>();
+        public bool Existence(int AccountNumber)
+        {
+            return UserInfo.ContainsKey(AccountNumber);
+        }
+        public void Deposit(int AccountNumber, decimal Amount)
+        {
+            try
+            {
+                UserInfo[AccountNumber].Balance += Amount;
+            }
+            catch (KeyNotFoundException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+
+        public void Withdraw(int AccountNumber, decimal Amount)
+        {
+            try
+            {
+                if (UserInfo[AccountNumber].Balance >= Amount)
+                {
+                    UserInfo[AccountNumber].Balance -= Amount;
+                    Console.WriteLine("Withdrawal successful!");
+                }
+                else
+                {
+                    throw new Exception("Insufficient balance!");
+                }
+            }
+            catch (KeyNotFoundException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+
+        public void CheckBalance(int AccountNumber)
+        {
+            try
+            {
+                Console.WriteLine("Account Holder Name: " + UserInfo[AccountNumber].Name);
+                Console.WriteLine("Account Balance: " + UserInfo[AccountNumber].Balance);
+            }
+            catch (KeyNotFoundException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+    }
+}
---
+++ BBS/BBS/SavingsAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBS
+{
+    class SavingsAccount : BankAccount
+    {
+        public void TakeInfo()
+        {
+            Console.Write("Enter Account Number: ");
+            int AccountNumber = Convert.ToInt32(Console.ReadLine());
+            //bool AlreadyExists = Existence(AccountNumber);
+            if (UserInfo.ContainsKey(AccountNumber))
+            {
+                throw new Exception("Account already exists!");
+            }
+            else
+            {
+                Account acc = new Account();
+                Console.Write("Enter account Holder Name: ");
+                acc.Name = Console.ReadLine();
+                Console.Write("Enter Initial Balance: ");
+                acc.Balance = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Enter Interest Rate (%): ");
+                acc.InterestRate = Convert.ToDouble(Console.ReadLine());
+                UserInfo.Add(AccountNumber, acc);
+                Console.WriteLine("Savings Account Created Sucessfully!\n");
+            }
+        }
+
+        public void ApplyInterest(int AccountNumber)
+        {
+            try
+            {
+                UserInfo[AccountNumber].Balance += UserInfo[AccountNumber].Balance * (decimal)UserInfo[AccountNumber].InterestRate / 100;
+                Console.WriteLine("Interest applied successfully!");
+            }
+            catch (KeyNotFoundException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+    }
+}
